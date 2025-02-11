6050503c069e4d4971e6fa0805b706fa7388ce39
Pat9780Bitcoin <patrick18483@google.com>
2025-02-11 16:01:17 -0500

    main: test, juan

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Acountable Account App',
+      title: 'Acountable Account',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
