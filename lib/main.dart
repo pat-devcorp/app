f78d31b8ee3ed089da5744b5a30472c85c0474cc
Pat9780Bitcoin <patrick18483@google.com>
2025-02-13 17:57:29 -0500

    chore(clean): drop widget files

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
-import 'package:app/Domain/Model/bootstrap.dart';
-import 'package:app/Presentation/views/pages/login.dart';
+import '/Domain/Model/bootstrap.dart';
+import '/Presentation/views/pages/login.dart';
+import 'Presentation/views/style/theme.dart';
+import 'Presentation/views/style/util.dart';
+import 'locator.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'locator.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   try {
     await dotenv.load(fileName: ".env"); // Load environment variables
   } catch (e) {
@@ -20,10 +22,24 @@ Future<void> main() async {
 
   runApp(
     ProviderScope(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: LoginPage(),
-      ),
+      child: MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final brightness = View.of(context).platformDispatcher.platformBrightness;
+    TextTheme textTheme = createTextTheme(context, "JetBrains Mono", "Roboto");
+    MaterialTheme theme = MaterialTheme(textTheme);
+
+    return MaterialApp(
+      theme: brightness == Brightness.light ? theme.light() : theme.dark(),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(), // Your home page
+    );
+  }
 }
