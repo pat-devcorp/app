088a28f8ad9fc9e278a2c66ad33941123e47524d
Kevin Sebastian Carrillo Varas <desarrollador.05.9780@gmail.com>
2025-02-18 15:38:30 -0500

    dialog custom

---
+++ lib/Domain/Model/transaction_list_model.dart
@@ -0,0 +1,23 @@
+class TransactionList {
+    int transactionId;
+    int amount;
+    int clientId;
+
+    TransactionList({
+        required this.transactionId,
+        required this.amount,
+        required this.clientId,
+    });
+
+    factory TransactionList.fromJson(Map<String, dynamic> json) => TransactionList(
+        transactionId: json["transactionId"],
+        amount: json["amount"],
+        clientId: json["client_id"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "transactionId": transactionId,
+        "amount": amount,
+        "client_id": clientId,
+    };
+}
